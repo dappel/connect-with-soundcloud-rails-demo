c68bb093d3c2fca986d166cdbc777d921f1b4a0f
Johannes Wagener <johannes@soundcloud.com>
2011-02-17 15:59:27 -0500

    fresh rails 3.0.4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConnectWithSoundCloudDemo::Application.config.secret_token = 'f7bdb35a3e2db1ba2edb65bc7afee53d4f2b22caf7b37a80c9406d57fc79cfaee741f2295e645c7aad6a00075c34d1e915df5d4f6c8e318630835ebef8182fbd'
